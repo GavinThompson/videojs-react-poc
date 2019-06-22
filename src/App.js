e821ec5eb665c2d463439148784a1ccc7f9906c0
Gavin Thompson <gavinathompson@gmail.com>
2019-06-22 17:27:35 -0400

    adding playlist; adding prettier; wiring up event listeners; currently works: handlePlaylistItem, handleBeforePlaylistItem, and videoEnded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Video from './Video'
-
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Video from "./Video";
 
 function App() {
-
   const videoJsOptions = {
     autoplay: true,
-    controls: true,
-    sources: [{
-      src: 'http://media.w3.org/2010/05/video/movie_300.mp4',
-      type: 'video/mp4'
-    }]
-  }
+    controls: true
+    // sources: [{
+    //   src: 'http://media.w3.org/2010/05/video/movie_300.mp4',
+    //   type: 'video/mp4'
+    // }]
+  };
+  const playlist = [
+    {
+      sources: [
+        {
+          src: "http://techslides.com/demos/sample-videos/small.mp4",
+          type: "video/mp4"
+        }
+      ]
+    },
+    {
+      sources: [
+        {
+          src: "http://techslides.com/demos/sample-videos/small.mp4",
+          type: "video/mp4"
+        }
+      ]
+    },
+    {
+      sources: [
+        {
+          src: "http://media.w3.org/2010/05/video/movie_300.mp4",
+          type: "video/mp4"
+        }
+      ]
+    },
+    {
+      sources: [
+        {
+          src: "http://vjs.zencdn.net/v/oceans.mp4",
+          type: "video/mp4"
+        }
+      ]
+    },
+    {
+      sources: [
+        {
+          src: "http://media.w3.org/2010/05/bunny/movie.mp4",
+          type: "video/mp4"
+        }
+      ]
+    },
+    {
+      sources: [
+        {
+          src: "http://media.w3.org/2010/05/video/movie_300.mp4",
+          type: "video/mp4"
+        }
+      ]
+    }
+  ];
 
   return (
     <div className="App">
-
-      <Video {...videoJsOptions}/>
-
+      <Video playlist={playlist} {...videoJsOptions} />
     </div>
   );
 }
--- src/Video.jsx
+++ src/Video.jsx
@@ -1,20 +1,66 @@
-import React from 'react';
-import videojs from 'video.js'
-import 'video.js/dist/video-js.css'
+import React from "react";
+import videojs from "video.js";
+import videojsPlaylistPlugin from "videojs-playlist";
 
+import "video.js/dist/video-js.css";
 
 export default class Video extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
   componentDidMount() {
     // instantiate Video.js
-    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
-      console.log('onPlayerReady', this)
+    // videojs.registerPlugin("playlist", videojsPlaylistPlugin);
+
+    this.player = videojs(this.videoNode, this.props, () => {
+      console.log("onPlayerReady this", this);
+
+      console.log(this.player);
+
+      this.player.on("ended", this.handleVideoEnded);
+      this.player.on("duringplaylistchange", this.handleDuringplaylistchange);
+      this.player.on("playlistchange", this.handlePlaylistchange);
+      this.player.on("beforeplaylistitem", this.handleBeforeplaylistitem);
+      this.player.on("playlistitem", this.handlePlaylistitem);
     });
+
+    this.player.playlist(this.props.playlist);
+    // this.player.playlist.first();
+    this.player.playlist.autoadvance(0);
   }
+
+  handleDuringplaylistchange = event => {
+    console.log("handleDuringplaylistchange", event);
+  };
+
+  handlePlaylistchange = event => {
+    console.log("handlePlaylistchange", event);
+  };
+
+  handleBeforeplaylistitem = event => {
+    console.log("handleBeforeplaylistitem", event);
+  };
+
+  handlePlaylistitem = event => {
+    console.log("handlePlaylistitem", event);
+  };
+
+  handleVideoEnded = event => {
+    console.log("handleVideoEnded", event);
+  };
+
+  handleVideoEnded = event => {
+    console.log("handleVideoEnded", event);
+  };
 
   // destroy player on unmount
   componentWillUnmount() {
     if (this.player) {
-      this.player.dispose()
+      this.player.dispose();
     }
   }
 
@@ -25,9 +71,12 @@ export default class Video extends React.Component {
     return (
       <div>
         <div data-vjs-player>
-          <video ref={ node => this.videoNode = node } className="video-js"></video>
+          <video
+            ref={node => (this.videoNode = node)}
+            className="video-js"
+          ></video>
         </div>
       </div>
-    )
+    );
   }
 }
