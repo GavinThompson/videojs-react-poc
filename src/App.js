367af6b85acbed221966ad028a1206ca373c6a3b
Gavin Thompson <gavinathompson@gmail.com>
2019-06-22 16:59:21 -0400

    adding video.js package

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Video from './Video'
+
+
 
 function App() {
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    sources: [{
+      src: 'http://media.w3.org/2010/05/video/movie_300.mp4',
+      type: 'video/mp4'
+    }]
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Video {...videoJsOptions}/>
+
     </div>
   );
 }
