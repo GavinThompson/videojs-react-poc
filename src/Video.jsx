b2a190b6e1851d46327b4e0c558e36b4f06d4d9f
Kevin Bache <kevin@medistream.ca>
2019-06-25 13:55:00 -0400

    add counter

--- src/Video.jsx
+++ src/Video.jsx
@@ -50,7 +50,7 @@ export default class Video extends React.Component {
     });
 
     this.player.playlist(this.props.playlist);
-    this.player.playlist.first();
+    // this.player.playlist.first();
     // this.player.playlist.autoadvance(0);
   }
 
@@ -73,30 +73,47 @@ export default class Video extends React.Component {
   handleVideoEnded = event => {
     console.log("meow");
     console.log("handleVideoEnded", event);
-    // console.log(this, this.player, this.player.playlist);
+    this.setState({counter: this.state.counter +1},() => {
+      console.log('counter', this.state.counter)
+      // console.log(this, this.player, this.player.playlist);
+      //
+      //
+      //
+      // if (this.state.counter %3 === 0) {
+      //   console.log('ad vid');
+      //   this.player.ima.setContentWithAdTag(
+      //     this.player.playlist.next(),
+      //     this.player.ima.adTagUrl = "https://rtbapi.campsiteproject.com/vast/2.0/loop?apiKey=91IYCBRb47serD7iBm6yYIRoxZXRai0hw2WVEhz4mCF09KPnH4gLFJ6SDwAiUivfngNi0pkL8F46lRji16LY1PWkMdzOqlO4HYvF&player%5Bid%5D=test-player&player%5Benvironment%5D=doctors-offices&loop%5BminDuration%5D=0&loop%5BmaxDuration%5D=60&loop%5BdwellTime%5D=20",
+      //     false
+      //   );
+      //   this.player.ima.requestAds();
+      // } else {
+      //   console.log('next vid no ad')
+      //   this.player.playlist.next();
+      // }
+    })
+
+    // console.log('ad vid');
+    // this.player.ima.setContentWithAdTag(
+    //   this.player.playlist.next(),
+    //   this.player.ima.adTagUrl = "https://rtbapi.campsiteproject.com/vast/2.0/loop?apiKey=91IYCBRb47serD7iBm6yYIRoxZXRai0hw2WVEhz4mCF09KPnH4gLFJ6SDwAiUivfngNi0pkL8F46lRji16LY1PWkMdzOqlO4HYvF&player%5Bid%5D=test-player&player%5Benvironment%5D=doctors-offices&loop%5BminDuration%5D=0&loop%5BmaxDuration%5D=60&loop%5BdwellTime%5D=20",
+    //   false
+    // );
+    // // this.player.ima.requestAds();
     //
+    // if (this.state.counter %3 === 0) {
+    //   console.log('ad vid');
+    //   this.player.ima.setContentWithAdTag(
+    //     this.player.playlist.next(),
+    //     this.player.ima.adTagUrl = "https://rtbapi.campsiteproject.com/vast/2.0/loop?apiKey=91IYCBRb47serD7iBm6yYIRoxZXRai0hw2WVEhz4mCF09KPnH4gLFJ6SDwAiUivfngNi0pkL8F46lRji16LY1PWkMdzOqlO4HYvF&player%5Bid%5D=test-player&player%5Benvironment%5D=doctors-offices&loop%5BminDuration%5D=0&loop%5BmaxDuration%5D=60&loop%5BdwellTime%5D=20",
+    //     false
+    //   );
+    //   this.player.ima.requestAds();
+    // } else {
+    //   console.log('next vid no ad')
+    //   this.player.playlist.next();
+    // }
 
-    this.player.ima.setContentWithAdTag(
-      this.player.playlist.next(),
-      this.player.ima.adTagUrl = "https://rtbapi.campsiteproject.com/vast/2.0/loop?apiKey=91IYCBRb47serD7iBm6yYIRoxZXRai0hw2WVEhz4mCF09KPnH4gLFJ6SDwAiUivfngNi0pkL8F46lRji16LY1PWkMdzOqlO4HYvF&player%5Bid%5D=test-player&player%5Benvironment%5D=doctors-offices&loop%5BminDuration%5D=0&loop%5BmaxDuration%5D=60&loop%5BdwellTime%5D=20",
-      false
-    );
-    this.player.contentSourceType = '' // NOTE: https://github.com/googleads/videojs-ima/issues/437 && https://github.com/googleads/videojs-ima/pull/433
-    this.player.ima.requestAds();
-
-    // TWO WAYS WORK -- above and below!
-    // this.player.ima.requestAds();
-    // this.player.playlist.next()
-
-
-
-    //     Function originally looks like this:
-    //
-    //     Controller.prototype.setContentWithAdTag =function(contentSrc, adTag, playOnLoad) {
-    //          this.reset();
-    //          this.settings.adTagUrl = adTag ? adTag : this.settings.adTagUrl;
-    //          this.playerWrapper.changeSource(contentSrc, playOnLoad);
-    //      };
   };
 
   // destroy player on unmount
@@ -110,6 +127,7 @@ export default class Video extends React.Component {
   // so videojs won't create additional wrapper in the DOM
   // see https://github.com/videojs/video.js/pull/3856
   render() {
+    console.log("RENDER")
     return (
       <div>
         <div data-vjs-player>
